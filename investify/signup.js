8cb42bfb227dc5e6a2970a655e56f4159866e0e1
Forester04 <erickforester04@gmail.com>
2024-11-22 23:43:57 +0300

    Investify mini-app

--- investify/signup.js
+++ investify/signup.js
@@ -2,37 +2,41 @@ const signupForm = document.getElementById('signup-form');
 const loginForm = document.getElementById('login-form');
 let users = []; // Array to store user data
 
+let globalEmail;
+let globalPassword;
+
+const result1 = globalEmail;
+const result2 = globalPassword
+
 // Sign up event listener
 signupForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const firstName = document.getElementById('signup-firstname').value;
   const lastName = document.getElementById('signup-lastname').value;
-  const email = document.getElementById('signup-email').value;
-  const password = document.getElementById('signup-password').value;
+  globalEmail = document.getElementById('signup-email').value;
+  globalPassword = document.getElementById('signup-password').value;
 
   // Basic validation
-  if (!firstName || !lastName || !email || !password) {
+  if (!firstName || !lastName || !globalEmail || !globalPassword) {
     alert('Please fill in all fields.');
     return;
   }
 
   // Check if user already exists
-  const existingUser = users.find(user => user.email === email);
+  const existingUser = users.find(user => user.globalEmail === globalEmail);
   if (existingUser) {
     document.getElementById("existingUser").textContent = "User already exists. Please log in.";
     return;
   }
 
-  users.push({ firstName, lastName, email, password });
+  users.push({ firstName, lastName, globalEmail, globalPassword });
   document.getElementById("success1").textContent = "Sign up successful!";
-
-  // Add new user to the array
   
   
 
   // Redirect to login page or other desired page
-  // window.location.href = 'index.html';
+ window.location.href = 'index.html';
 });
 
 
@@ -42,9 +46,8 @@ loginForm.addEventListener('submit', (event) => {
   const password = document.getElementById('login-password').value;
 
   // Find the user in the array
-  const user = users.find(user => user.email === email && user.password === password);
 
-  if (user) {
+  if ( result1 === globalEmail && result2 === globalPassword) {
     // Successful login, redirect to profile page or other protected area
     document.getElementById("success2").textContent = "Sign up successful!"
     window.location.href = 'profile.html'; // Replace with your actual profile page URL
