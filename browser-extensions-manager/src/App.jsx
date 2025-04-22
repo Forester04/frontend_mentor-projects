744a75fbc7ee4c1687fadd25485aa1521e7c4191
Forester04 <erickforester04@gmail.com>
2025-04-22 17:00:35 +0300

    code update

--- browser-extensions-manager/src/App.jsx
+++ browser-extensions-manager/src/App.jsx
@@ -1,8 +1,11 @@
 
 import { useState } from 'react'
-import data from './data/data.json'
-import logo from './assets/images/logo-output.svg'
-import iconSun from './assets/images/icon-sun.svg'
+import data from './data.json'
+import darkLogo from './assets/images/logo-output.svg'
+import lightLogo from './assets/images/logo.svg'
+import darkIcon from './assets/images/icon-sun.svg'
+import lightIcon from './assets/images/icon-moon.svg'
+
 import './App.css'
 //import { preview } from 'vite'
 
@@ -13,6 +16,8 @@ function App() {
 
 
   const [extensionData, setExtensionData] = useState(data)
+
+  const [theme, setTheme] = useState("dark")
 
   const filteredData = extensionData.filter(extension => {
     if (selectedFilter === "all") return true;
@@ -49,6 +54,10 @@ function App() {
     )
   })
 
+  function handleTheme() {
+    setTheme(preTheme => preTheme === "dark" ? "light" : "dark")
+  }
+
   function handleToggle(name) {
     setExtensionData(prevExtension => prevExtension.map(exten => exten.name === name ? {...exten, "isActive": !exten.isActive} : exten))
     console.log("Changed")
@@ -58,38 +67,42 @@ function App() {
     setExtensionData(prevExtension => prevExtension.filter(objArray => objArray.name !== name))
   }
 
-  
-
   return (
-    <main>
-      <header>
-        <div>
-          <img src={logo} alt="Extensions logo" className='logo' aria-label='Extensions logo'/>
-        </div>
-        <div className="theme">
-          <img src={iconSun} alt="light mode theme icon" aria-label="light theme mode"/>
-        </div>
-      </header>
-      <section className='list-selection'>
-        <h1>Extensions List</h1>
-        <div className="btn-selection">
-          <button
-            onClick={() => setSelectedFilter("all")}
-            className={selectedFilter === "all" ? "active-btn" : ""} 
-          >All</button>
-          <button
-            className={selectedFilter === "active" ? "active-btn" : ""} 
-            onClick={() => setSelectedFilter("active")}
-          >Active</button>
-          <button
-            className={selectedFilter === "inactive" ? "active-btn" : ""} 
-            onClick={() => setSelectedFilter("inactive")}
-          >Inactive</button>
-        </div>
-      </section>
-      <section className='card-section'>
-        {extensionElement}
-      </section>
+    <main className={ theme === "light" ? "light" : ""}>
+      <div className="wrapper">
+        <header>
+          <div>
+            { theme === "dark" ? <img src={darkLogo} alt="Extensions logo" className='logo' aria-label='Extensions logo'/> : <img src={lightLogo} alt="Extensions logo" className='logo' aria-label='Extensions logo'/> }
+          </div>
+          <div className="theme" onClick={ () => handleTheme()}>
+            { theme === "light" ? 
+                <img src={lightIcon} alt="light mode theme icon" aria-label="light mode theme"/>: 
+                <img src={darkIcon} alt="Dark mode theme icon" aria-label="Dark mode theme"/>  
+          }
+            
+          </div>
+        </header>
+        <section className='list-selection'>
+          <h1>Extensions List</h1>
+          <div className="btn-selection">
+            <button
+              onClick={() => setSelectedFilter("all")}
+              className={selectedFilter === "all" ? "active-btn" : ""} 
+            >All</button>
+            <button
+              className={selectedFilter === "active" ? "active-btn" : ""} 
+              onClick={() => setSelectedFilter("active")}
+            >Active</button>
+            <button
+              className={selectedFilter === "inactive" ? "active-btn" : ""} 
+              onClick={() => setSelectedFilter("inactive")}
+            >Inactive</button>
+          </div>
+        </section>
+        <section className='card-section'>
+          {extensionElement}
+        </section>
+      </div>
     </main>
   )
 }
--- browser-extensions-manager/vite.config.js
+++ browser-extensions-manager/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/frontend_mentor-projects/browser-extensions-manager"
+  plugins: [react()]
 })
